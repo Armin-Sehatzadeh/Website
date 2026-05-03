326d818197ec3b9bb5e961810cefe636454a4b84
Armin <arminsehatzadeh@gmail.com>
2026-05-03 10:11:21 +0330

    init databse students

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import Integer, String
+from datetime import date
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from sqlalchemy import Integer, String, ForeignKey, Float, Date
 
 class Base(DeclarativeBase):
     pass
@@ -14,5 +15,21 @@ class User(db.Model):
     first_name: Mapped[str] = mapped_column(String(255), nullable=False)
     last_name: Mapped[str] = mapped_column(String(255), nullable=False)
     role: Mapped[str] = mapped_column(String(20), nullable=False)
-    email: Mapped[str] = mapped_column(String(255), nullable=False, unique=True)
+    email: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(String(255), nullable=False)
+
+    student = relationship("Student", back_populates="user", uselist=False)
+
+
+class Student(db.Model):
+    __tablename__ = "students"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), unique=True, nullable=False)
+    phone_number: Mapped[str] = mapped_column(String(20))
+    birth_date: Mapped[date] = mapped_column(Date)
+    address: Mapped[str] = mapped_column(String(255))
+    score: Mapped[float] = mapped_column(Float)
+    grade_level: Mapped[int] = mapped_column(Integer, nullable=False)
+
+    user = relationship("User", back_populates="student")
