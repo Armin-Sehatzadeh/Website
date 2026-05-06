4484f33ba102bd7a50b516bbc8e9e9f3f0dcae6f
Armin <arminsehatzadeh@gmail.com>
2026-05-06 09:38:04 +0330

    Get Score And Student Profile For Student Api

--- My-Website/api/student.py
+++ My-Website/api/student.py
@@ -7,6 +7,7 @@ from flask_jwt_extended import get_jwt_identity
 student_bp = Blueprint("student_bp", __name__, url_prefix="/api/student")
 
 
+# GET SCORE
 @student_bp.get("/score")
 @jwt_required()
 @role_required("student")
@@ -18,9 +19,42 @@ def get_score():
     ).scalar_one_or_none()
 
     if not student:
-        return jsonify({"msg": "Student not found"}), 404
+        return jsonify({
+            "status": "fail",
+            "msg": "Student not found"
+        }), 404
 
     return jsonify({
+        "first_name": student.first_name,
+        "last_name": student.last_name,
         "score": student.score,
         "grade_level": student.grade_level
+    }), 200
+
+
+# STUDENT PROFILE
+@student_bp.get("profile")
+@jwt_required()
+@role_required("student")
+def student_profile():
+    
+    user_id = get_jwt_identity()
+    student = db.session.execute(
+        db.select(Student).where(Student.user_id == user_id)
+        ).scalar_one_or_none() 
+    
+    if not student:
+        return jsonify({
+            "status": "fail",
+            "msg": "Student not found"            
+        }), 404
+
+    return jsonify({
+        "first_name": student.first_name,
+        "last_name": student.last_name,       
+        "phone_number": student.phone_number,
+        "birth_date": student.birth_date,
+        "address": student.address,
+        "grade_level": student.grade_level,
+        "score": student.score
     }), 200
