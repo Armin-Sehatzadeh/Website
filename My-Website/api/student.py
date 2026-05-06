b2b509d583197c1edd078ccf2ad22d89d953a48f
Armin <arminsehatzadeh@gmail.com>
2026-05-06 09:54:17 +0330

    Adding Patch to Student Api

--- My-Website/api/student.py
+++ My-Website/api/student.py
@@ -1,8 +1,7 @@
-from flask import Blueprint, jsonify
-from flask_jwt_extended import jwt_required, get_jwt
+from flask import Blueprint, jsonify, request
+from flask_jwt_extended import jwt_required, get_jwt_identity
 from database import db, Student
 from decorators import role_required
-from flask_jwt_extended import get_jwt_identity
 
 student_bp = Blueprint("student_bp", __name__, url_prefix="/api/student")
 
@@ -33,7 +32,7 @@ def get_score():
 
 
 # STUDENT PROFILE
-@student_bp.get("profile")
+@student_bp.get("/profile")
 @jwt_required()
 @role_required("student")
 def student_profile():
@@ -57,4 +56,49 @@ def student_profile():
         "address": student.address,
         "grade_level": student.grade_level,
         "score": student.score
+    }), 200
+    
+    
+# EDIT STUDENT PROFILE    
+@student_bp.patch("/profile")
+@jwt_required()
+@role_required("student")
+def edit_profile():
+    
+    user_id = get_jwt_identity()
+    data = request.get_json() or {}
+    
+    student = db.session.execute(
+        db.select(Student).where(Student.user_id == user_id)
+        ).scalar_one_or_none()
+    
+    if not student:
+        return jsonify({
+            "status": "fail",
+            "msg": "Student not found"            
+        }), 404
+
+    allowed_fields = [
+        "phone_number",
+        "birth_date",
+        "address",
+        "grade_level"
+    ]
+
+    for field in allowed_fields:
+        if field in data:
+
+            if data[field] is None or str(data[field]).strip() == "":
+                return jsonify({
+                    "status": "fail",
+                    "msg": f"{field} cannot be empty".title()
+                }), 400
+
+            setattr(student, field, data[field])
+
+    db.session.commit()
+    
+    return jsonify({
+        "status": "success",
+        "msg": "Profile updated successfully"
     }), 200
