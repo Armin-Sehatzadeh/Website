d7bb0c28c287be1447d5aa2c8ab92cd6c7f9fe56
Armin <arminsehatzadeh@gmail.com>
2026-05-06 10:10:46 +0330

    Adding Profile Api For Teacher

--- My-Website/api/teacher.py
+++ My-Website/api/teacher.py
@@ -1,5 +1,33 @@
 from flask import request, jsonify, Blueprint
 from flask_jwt_extended import get_jwt_identity, jwt_required
-from database import User, Teacher
+from database import db, Teacher
 from decorators import role_required
 
+teacher_bp = Blueprint("teacher_bp", __name__, url_prefix="/api/teacher")
+
+
+# TEACHER PROFILE INFO
+@teacher_bp.get("/profile")
+@jwt_required()
+@role_required("teacher")
+def teacher_profile():
+    
+    user_id = get_jwt_identity()
+    teacher = db.session.execute(
+        db.select(Teacher).where(Teacher.user_id == user_id)
+        ).scalar_one_or_none() 
+    
+    if not teacher:
+        return jsonify({
+            "status": "fail",
+            "msg": "Teacher not found"            
+        }), 404
+
+    return jsonify({
+        "first_name": teacher.first_name,
+        "last_name": teacher.last_name,       
+        "phone_number": teacher.phone_number,
+        "birth_date": teacher.birth_date,
+        "address": teacher.address,
+    }), 200
+    
