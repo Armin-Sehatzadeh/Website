adcc1511377324b50c3dbb1cd8ef20edc19d5a1c
Armin <arminsehatzadeh@gmail.com>
2026-05-07 09:53:33 +0330

    Add teacher students list API

--- My-Website/api/teacher.py
+++ My-Website/api/teacher.py
@@ -1,6 +1,6 @@
 from flask import request, jsonify, Blueprint
 from flask_jwt_extended import get_jwt_identity, jwt_required
-from database import db, Teacher
+from database import db, Teacher, Student
 from decorators import role_required
 
 teacher_bp = Blueprint("teacher_bp", __name__, url_prefix="/api/teacher")
@@ -74,3 +74,41 @@ def edit_profile():
         "status": "success",
         "msg": "Profile updated successfully"
     }), 200
+    
+
+# TEACHERS STUDENTS
+@teacher_bp.get("/students")
+@jwt_required()
+@role_required("teacher")
+def get_students():
+
+    user_id = get_jwt_identity()
+
+    teacher = db.session.execute(
+        db.select(Teacher).where(Teacher.user_id == user_id)
+    ).scalar_one_or_none()
+
+    if not teacher:
+        return jsonify({"status": "fail", "msg": "Teacher not found"}), 404
+    
+    users = db.session.execute(
+    db.select(Student).where(Student.teacher_id == user_id)
+    ).scalars().all()
+    
+    students = []
+
+    for user in users:
+        students.append({
+            "id": user.id,
+            "name": f"{user.user.first_name} {user.user.last_name}",
+            "grade_level": user.grade_level,
+            "score": user.score
+        })
+        
+    return jsonify({
+        "status": "success",
+        "students": students
+    }), 200
+
+
+    
