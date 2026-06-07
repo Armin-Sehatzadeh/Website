0c6310e8da76d44c9fc38cede861580c24024791
Armin <arminsehatzadeh@gmail.com>
2026-06-07 14:33:56 +0330

    adding .env

--- main.py
+++ main.py
@@ -1,23 +1,32 @@
+import os
 from flask import Flask, render_template
-from database import db, TokenBlocklist
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
+from dotenv import load_dotenv
 
+from database import db, TokenBlocklist
 from api.auth import auth_bp
 from api.student import student_bp
 from api.teacher import teacher_bp
 from api.manager import manager_bp
 
+load_dotenv()
+
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config["JWT_SECRET_KEY"] = "super-secret-key"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
 
 db.init_app(app)
 
 jwt = JWTManager(app)
-CORS(app, resources={r"/api/*": {"origins": "*"}}, methods=["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"])
+
+CORS(
+    app,
+    resources={r"/api/*": {"origins": "*"}},
+    methods=["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"]
+)
 
 app.register_blueprint(auth_bp, url_prefix="/api/auth")
 app.register_blueprint(student_bp, url_prefix="/api/student")
@@ -31,35 +40,39 @@ def check_if_token_revoked(jwt_header, jwt_payload: dict) -> bool:
     token = db.session.query(TokenBlocklist.id).filter_by(jti=jti).scalar()
     return token is not None
 
-# ── Page Routes ──
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
+
 @app.route('/login')
 def login_page():
     return render_template('login.html')
+
 
 @app.route('/register')
 def register_page():
     return render_template('register.html')
 
+
 @app.route('/student-dashboard')
 def student_dashboard():
     return render_template('student.html')
 
+
 @app.route('/teacher-dashboard')
 def teacher_dashboard():
     return render_template('teacher.html')
+
 
 @app.route('/manager-dashboard')
 def manager_dashboard():
     return render_template('manager.html')
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     with app.app_context():
         db.create_all()
 
-    app.run(debug=True)
+    app.run(debug=os.getenv("FLASK_DEBUG") == "True")
